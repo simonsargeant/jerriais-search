05c7f8d635c529cbcb5628e5876fc95214d052c8
Simon Sargeant <simonashleysargeant@gmail.com>
2024-05-12 18:37:04 +0200

    Add production build config

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,11 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/jerriais-search/' : '/'
   plugins: [
     vue(),
     vueJsx(),
-    VueDevTools(),
+    //VueDevTools(),
   ],
   resolve: {
     alias: {
